7c41957de59c1769659d2f1a6e3231547a614391
Willian Rodrigues de Melo <willianr.demelo@gmail.com>
2020-08-19 12:01:03 -0300

    first commit

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<?php
+
+ 
+
+echo "Hello, World from Docker! <br>";
+echo "Hello ECS! <br>";
+echo "version: 00 <br>";
+echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+
+ 
+
+?>
