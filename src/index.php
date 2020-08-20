25aed0e55b0ff030eb6999463ae7e79623ba8901
Willian Rodrigues de Melo <willianr.demelo@gmail.com>
2020-08-20 16:58:04 -0300

    teste git

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@
 
 echo "Hello, World from Docker! <br>";
 echo "Hello ECS! <br>";
-echo "version: 00 <br>";
+echo "version: 01 <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
  
